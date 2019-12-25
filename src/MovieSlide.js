f41b221157755c51c294dd917372dff4940cb6fc
Johnny Hoang <johnnyhoaang@hotmail.com>
2019-12-25 15:33:49 +0100

    Added more pictures to Movieslide, created a onClick event for

--- src/MovieSlide.js
+++ src/MovieSlide.js
@@ -1,36 +1,45 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 
 
 function ControlledCarousel(testmovie) {
-    const [index, setIndex] = useState(0);
-    const [direction, setDirection] = useState(null);
-    console.log("assad", testmovie);
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-      setDirection(e.direction);
-    };
-  
-    return (
-      
-      <Carousel activeIndex={index} direction={direction} onSelect={handleSelect} interval={null}>
-        <Carousel.Item>
-          <img
-            className=""
-            src={testmovie.property.Poster}
-          
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={testmovie.property.Poster}/>
-  
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src = {testmovie.property.Poster}       />
-
-        </Carousel.Item>
-      </Carousel>
-    );
+  const imageClick = (event) => {
+    console.log(event.target.id);
   }
-  export default ControlledCarousel;
+
+  const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState(null);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+    setDirection(e.direction);
+  };
+
+
+
+  return (
+
+    <Carousel activeIndex={index} direction={direction} onSelect={handleSelect} interval={null}>
+      <Carousel.Item>
+        <img id="1" src={testmovie.property.Poster} onClick={imageClick} />
+        <img id="2" src={testmovie.property.Poster} onClick={imageClick} />
+        <img id="3" src={testmovie.property.Poster} onClick={imageClick} />
+
+      </Carousel.Item>
+      <Carousel.Item>
+        <img id="4" src={testmovie.property.Poster} onClick={imageClick} />
+        <img id="5" src={testmovie.property.Poster} onClick={imageClick} />
+        <img id="6" src={testmovie.property.Poster} onClick={imageClick} />
+
+
+      </Carousel.Item>
+      <Carousel.Item>
+        <img id="7" src={testmovie.property.Poster} onClick={imageClick} />
+        <img id="8" src={testmovie.property.Poster} onClick={imageClick} />
+        <img id="9" src={testmovie.property.Poster} onClick={imageClick} />
+
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+export default ControlledCarousel;
